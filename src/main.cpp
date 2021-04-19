9ca83f273b7d39f7ef06b2dadc3c4a6096753c8a
blaise-rascal <65874622+blaise-rascal@users.noreply.github.com>
2021-04-18 22:32:38 -0400

    number ticks up when player presses a button

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "bn_sprite_ptr.h"
 #include "bn_sprites.h"
 #include "bn_vector.h"
+#include "bn_keypad.h"
 #include "bn_string.h"
 #include "bn_sprite_text_generator.h"
 
@@ -25,22 +26,31 @@ int main()
     bn::sprite_ptr bomb_sprite = bn::sprite_items::hero_bomb_icon.create_sprite(10, 0);
 
     //bf::status status;
-    bn::string<20> weight_hud_text("WEIGHT: 0/4");
-    int current_weight = 4;
-    //high_experience_text.append(bn::to_string<8>(status.high_experience()));
+    bn::string<20> weight_hud_text("NUMBER PRESSES: ");
+    int current_weight = 0;
+    weight_hud_text.append(bn::to_string<8>(current_weight));
 
-    //const int 
+ 
     bn::sprite_text_generator my_text_generator(variable_8x8_sprite_font);
     my_text_generator.set_left_alignment();
     //my_text_generator.set_bg_priority(1); 
     
     bn::vector<bn::sprite_ptr, 32> text_sprites;
 
-    my_text_generator.generate(0, 0, weight_hud_text, text_sprites);
+    my_text_generator.generate(-100, 0, weight_hud_text, text_sprites);
 
 
     while(true)
     {
+        if(bn::keypad::a_pressed())
+            {
+                text_sprites.clear();
+                current_weight++;
+                weight_hud_text="NUMBER PRESSES: ";
+                weight_hud_text.append(bn::to_string<8>(current_weight));
+                my_text_generator.generate(-100, 0, weight_hud_text, text_sprites);
+
+            }
         bn::core::update();
     }
 }
